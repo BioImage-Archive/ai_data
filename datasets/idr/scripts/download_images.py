5cfaf86cd059776047e6170611e6f0f2b2308f38
Craig Russell <craig.russell.phd@gmail.com>
2023-11-21 12:52:48 +0000

    fixed idr funs

--- datasets/idr/scripts/download_images.py
+++ datasets/idr/scripts/download_images.py
@@ -1,11 +1,10 @@
-from ..idr_funs import download_image
+from idr_funs import download_image
 from idr import connection
 # Import necessary modules
 import omero
 import os
 from tqdm import tqdm
 import json
- 
 conn = connection("idr.openmicroscopy.org")
 download_image(
     conn,
