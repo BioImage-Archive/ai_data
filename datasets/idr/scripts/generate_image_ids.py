08e9a8f7fc171a5dbeef066cafdb2c94652a3893
Craig Russell <craig.russell.phd@gmail.com>
2024-02-01 12:16:18 +0000

    Adding idr scripts (might cull later)

---
+++ datasets/idr/scripts/generate_image_ids.py
@@ -0,0 +1,26 @@
+# Import necessary modules
+import omero
+import os
+import json 
+import idr_funs 
+from idr import connection
+breakpoint()
+conn = connection("idr.openmicroscopy.org")
+# image_ids = get_image_ids
+screen = conn.getObject('Screen', attributes={'name': snakemake.wildcards.screen_name})
+# screens = conn.getObject('Screen', attributes={'name': wildcards.screen_name})
+print("Getting plates")
+plates = idr_funs.get_omero_children(screen)
+print("Getting wells")
+wells = idr_funs.get_children_from_list_of_objects(plates)
+print("Getting imageids")
+well_sampler = idr_funs.get_children_from_list_of_objects(wells)
+# breakpoint()
+# print("Getting plates")
+image_ids = [x.image().id for x in well_sampler]
+with open(snakemake.output.image_ids, "w") as f:
+    json.dump(image_ids, f)
+# with open(output.image_ids, "w") as f:
+#     f.write("\n".join(str(x) for x in image_ids))
+print("generate_image_ids: Done")
+conn._closeSession()
