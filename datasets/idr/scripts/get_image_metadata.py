be8b43c73d504c34ddde6208292d9e1b847ea3d0
Craig Russell <craig.russell.phd@gmail.com>
2024-02-29 09:21:38 +0000

    Adding idr download script

--- datasets/idr/scripts/get_image_metadata.py
+++ datasets/idr/scripts/get_image_metadata.py
@@ -1,23 +1,31 @@
 from idr import connection
+
 # Import necessary modules
 import omero
 import os
 from tqdm import tqdm
-import json 
+import json
 
 conn = connection("idr.openmicroscopy.org")
-with open(snakemake.input.image_ids, 'r') as f:
+with open(snakemake.input.image_ids, "r") as f:
     image_ids = json.load(f)
 # with open(input.image_ids) as f:
-    # breakpoint()
-    # image_ids = [int(x.strip()) for x in f.readlines()]
+# breakpoint()
+# image_ids = [int(x.strip()) for x in f.readlines()]
 metadata = {}
 for image_id in tqdm(image_ids):
     image = conn.getObject("Image", image_id)
     z = image.getSizeZ()
     c = image.getSizeC()
     t = image.getSizeT()
-    metadata[str(image_id)] = {"z": z, "c": c, "t": t}
+    screen_name = snakemake.wildcards.screen_name
+    image_id = int(image_id)
+    metadata[str(image_id)] = {
+        "screen_id": screen_name,
+        "z": z,
+        "c": c,
+        "t": t,
+    }
 
 with open(snakemake.output.metadata, "w") as f:
     json.dump(metadata, f)
