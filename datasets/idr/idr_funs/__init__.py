b70bbd80630d89e62d2f0db6b6be348e4e1ecbf0
Craig Russell <craig.russell.phd@gmail.com>
2024-04-19 13:23:05 +0100

    Forgot to update idr funs

--- datasets/idr/idr_funs/__init__.py
+++ datasets/idr/idr_funs/__init__.py
@@ -2,9 +2,14 @@ from pathlib import Path
 from idr import connection, images
 from tqdm import tqdm
 from PIL import Image
+import omero
+
+IDR_PATH = "idr.openmicroscopy.org"
+
 
 def get_omero_children(blitz_object):
     return [child for child in blitz_object.listChildren()]
+
 
 def get_children_from_list_of_objects(blitz_objects):
     children = []
@@ -12,13 +17,33 @@ def get_children_from_list_of_objects(blitz_objects):
         children.extend(get_omero_children(obj))
     return children
 
+
+def get_omero_children_id(omero_id, field):
+    conn = connection("idr.openmicroscopy.org",verbose=-1)
+    child_id = get_omero_child_id_unconnected(omero_id, field, conn)
+    conn._closeSession()
+    return child_id
+
+
+def get_omero_id(omero_id, field, conn):
+    return conn.getObject(field, omero_id)
+
+
+def get_omero_child_id_unconnected(omero_id, field, conn):
+    blitz_object = conn.getObject(field, omero_id)
+    ids = []
+    for child in blitz_object.listChildren():
+        ids.append(child.getId())
+    return ids
+
+
 # def get_plates_by_screen(conn,screen_name):
 #     # Get the screen object
 #     screen = conn.getObject('Screen', attributes={'name': screen_name})
 
 #     # Get all the image objects in the screen
 #     return get_omero_children(screen)
-    
+
 # def get_image_ids_by_screen(conn,screen_name):
 #     screen = conn.getObject('Screen', attributes={'name': screen_name})
 #     plates = get_children_from_list_of_objects([screen])
@@ -36,9 +61,10 @@ def get_children_from_list_of_objects(blitz_objects):
 # Create the output directory if it doesn't exist
 # Path(output_dir).mkdir(parents=True, exist_ok=True)
 
+
 # Define a function to download images
-def download_image(conn, image_id, output_file,z=0,c=0,t=0):
-    image = conn.getObject('Image', image_id)
+def download_image(conn, image_id, output_file, z=0, c=0, t=0):
+    image = conn.getObject("Image", image_id)
     pixels = image.getPrimaryPixels()
     plane = pixels.getPlane(z, c, t)
     print(f"Saving image {image_id} to {output_file}")
@@ -47,9 +73,41 @@ def download_image(conn, image_id, output_file,z=0,c=0,t=0):
     return im
     # images.download_image(conn, image_id, download_path=output_dir)
 
+
 def get_image_dimensions(conn, image_id):
-    image = conn.getObject('Image', image_id)
+    image = conn.getObject("Image", image_id)
     z = image.getSizeZ()
     c = image.getSizeC()
     t = image.getSizeT()
     return z, c, t
+
+
+def get_hcs(idr_path=IDR_PATH):
+    # breakpoint()
+    conn = connection(idr_path, verbose=0)
+    screens = conn.getObjects("Screen")
+    filtered_screens = []
+    for screen in screens:
+        # Get all the map annotation associated with the screen
+        map_anns = [
+            ann
+            for ann in screen.listAnnotations()
+            if isinstance(ann, omero.gateway.MapAnnotationWrapper)
+        ]
+        # Filter based on the map annotation values
+        for ann in map_anns:
+            values = dict(ann.getValue())
+            if (
+                values.get("Organism") == "Homo sapiens"
+                and values.get("Study Type") == "high content screen"
+            ):
+                filtered_screens.append(screen.name)
+    conn.close()
+    return filtered_screens
+
+
+def get_metadata_unconnected(image_id, screen_name):
+    conn = connection("idr.openmicroscopy.org", verbose=0)
+    metatdata = get_metadata_connected(image_id, screen_name, conn)
+    conn._closeSession()
+    return metatdata
